a697a9abfe709257471ea81f39e6d229ddfd00f6
intelligentdnb <juandiarts@gmail.com>
2023-02-09 23:30:54 -0300

    feat: Header and Navbar completed

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -8,15 +8,24 @@ const Header = () => {
                 <div className="card card-body bg-secondary text-light">
                     <div className="row">
                         <div className="col-md-4">
-                            <img src="/assets/mejd.jpeg" className="img-fluid" alt="me" style={{borderRadius: 25, border: "1px solid black"}}/>
+                            <img src="/assets/mejd.jpeg" className="img-fluid" alt="me" style={{ borderRadius: 25, border: "1px solid black" }} />
                         </div>
                         <div className="col-md-8">
-                            <h3>Juan Diego MCA</h3>
-                            <h5>Developer</h5>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                                Doloribus quae deserunt neque qui eaque fuga soluta nemo vero aliquam harum ad, pariatur quidem quos 
-                                unde placeat animi alias nam! Fugiat! </p>
-                            <Link href="/contact" style={{color: "lightblue"}}>Contact</Link>
+                            <h2>Juan Diego MCA</h2>
+                            <h5>Software Developer</h5>
+                            <p>I study to build solutions that help other people as well as myself. I enjoy learning new languages and
+                                frameworks. Interested in the data, how to present it, transmit it and its respective security. I want people
+                                to become financially empowered. </p>
+                            <div className="card text-white bg-primary mb-3">
+                                <div className="card-body">
+                                    <h5>Contact </h5>
+                                    
+                                    <p className="card-text">Email: <a href="mailto:juandiarts@gmail.com" style={{ color: "white", }}>juandiarts@gmail.com</a></p>
+                                    <p className="card-text">Tel: <a href="tel:+5493764410695" style={{ color: "white", }}>+54 9 3764 41 0695</a>   | Posadas, Misiones, Argentina.</p>
+                                    <p className="card-text"><a href="https://www.linkedin.com/in/juandimca" target="_blank" style={{ color: "white", }}>linkedin.com/in/juandimca</a> | <a href="https://github.com/intelligentdnb" style={{ color: "white", }}>github.com/intelligentdnb</a></p>
+                                    <b>Languagues:</b> <p>English advanced <a href="https://www.efset.org/cert/UhnwFb" style={{ color: "white", }} target="_blank">C1 certificate.</a> | Spanish native.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,24 +5,19 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <Link className="navbar-brand" href="/">JD MCA</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" href="/blog">
+                            <Link className="nav-link" aria-current="page" href="/portfolioPage">
                                 Portfolio
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">GitHub</Link>
+                            <Link className="nav-link" href="https://github.com/intelligentdnb" target="_blank">GitHub</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">Contact</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="#">Linkedin</Link>
+                            <Link className="nav-link" href="https://www.linkedin.com/in/juandimca/" target="_blank">Linkedin</Link>
                         </li>
                     </ul>
                 </div>
