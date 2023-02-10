5fcd7248282a0acd0dcb8ba317c49f4073f031d9
intelligentdnb <juandiarts@gmail.com>
2023-02-09 21:44:44 -0300

    feat: examples of assets and profile portfolio info

---
+++ profile.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        name: "proyecto 1",
+        description: "Lorem, asdasdadsadasdasdaAAAAAAAAAAAAAAAAAAAAAAdadad",
+        image: "/assets/portfolio1.jpeg",
+        link: ""
+    },
+    {
+        name: "proyecto 2",
+        description: "Lorem, asdasdadsadasdasdaAAAAAAAAAAAAAAAAAAAAAAdadad",
+        image: "/assets/portfolio2.jpg",
+        link: ""
+    },
+    {
+        name: "proyecto 3",
+        description: "Loremssssssssssssssssssssssssssssssadasda sdasdasd asd asdasd",
+        image: "/assets/portfolio3.png",
+        link: ""
+    },
+    {
+        name: "proyecto 4",
+        description: "Lorem, asdasdadsadasdasdaAAAAAAAAAAAAAAAAAAAAAAdadad",
+        image: "/assets/portfolio4.png",
+        link: ""
+    },
+    {
+        name: "proyecto 5",
+        description: "Lorem, asdasdadsadasdasdaAAAAAAAAAAAAAAAAAAAAAAdadad",
+        image: "/assets/portfolio5.jpeg",
+        link: ""
+    },
+    {
+        name: "proyecto 6",
+        description: "Lorem, asdasdadsadasdasdaAAAAAAAAAAAAAAAAAAAAAAdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadad",
+        image: "/assets/portfolio6.jpeg",
+        link: ""
+    },
+]
