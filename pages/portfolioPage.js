98329426bc7490ffcc4237efebe89653c032dc6a
intelligentdnb <juandiarts@gmail.com>
2023-02-13 20:55:58 -0300

    fix: portfolio page description link

--- pages/portfolioPage.js
+++ pages/portfolioPage.js
@@ -13,7 +13,7 @@ const PortfolioPage = () => {
                                 <h1 className="text-center text-light">Portfolio</h1>
                             </div>
                             {
-                                projects.map(({ name, description, image, link }, i) => (
+                                projects.map(({ name, description, image, link, descripLink }, i) => (
                                     <div className="col-md-4 p-2" key={i}>
                                         <div className="card h-100">
                                             <div className="overflow">
@@ -22,7 +22,7 @@ const PortfolioPage = () => {
                                             <div className="card-body bg-dark text-light" >
                                                 <h3>{name}</h3>
                                                 <p>{description}</p>
-                                                <a href={link} className="text-light">Know more</a>
+                                                <a href={link} className="text-light">{descripLink}</a>
                                             </div>
                                         </div>
                                     </div>
